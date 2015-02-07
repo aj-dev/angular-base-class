1d1ff5121f3e7667eefc1867a2bdba5bbe7b2037
aj-dev <reskogitas@gmail.com>
2015-02-07 23:23:09 +0100

    Reformated

--- gruntfile.js
+++ gruntfile.js
@@ -1,43 +1,43 @@
 /* globals module:false, require:false */
 module.exports = function (grunt) {
 
-	'use strict';
+    'use strict';
 
-	// load all grunt tasks matching the 'grunt-*' pattern
-	require('load-grunt-tasks')(grunt);
+    // load all grunt tasks matching the 'grunt-*' pattern
+    require('load-grunt-tasks')(grunt);
 
-	grunt.initConfig({
-		karma: {
-			unit: {
-				configFile: 'karma.conf.js',
-				singleRun: true
-			},
-			auto: {
-				configFile: 'karma.conf.js',
-				autoWatch: true
-			}
-		},
-		uglify: {
-			options: {
-				mangle: true
-			},
-			target: {
-				files: {
-					'dist/angular-base-class.min.js': 'src/angular-base-class.js'
-				}
-			}
-		},
-		bump: {
-			scripts: {
-				files: ['package.json', 'bower.json'],
-				commitFiles: ['-a'],
-				push: false
-			}
-		}
-	});
+    grunt.initConfig({
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            },
+            auto: {
+                configFile: 'karma.conf.js',
+                autoWatch: true
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true
+            },
+            target: {
+                files: {
+                    'dist/angular-base-class.min.js': 'src/angular-base-class.js'
+                }
+            }
+        },
+        bump: {
+            scripts: {
+                files: ['package.json', 'bower.json'],
+                commitFiles: ['-a'],
+                push: false
+            }
+        }
+    });
 
-	// Load all modules from package.json which name starts with 'grunt-'. Very helpful to avoid having to loadNpmTasks for every grunt module
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // Load all modules from package.json which name starts with 'grunt-'. Very helpful to avoid having to loadNpmTasks for every grunt module
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-	grunt.registerTask('test', ['karma:unit']);
+    grunt.registerTask('test', ['karma:unit']);
 };
--- src/angular-base-class.js
+++ src/angular-base-class.js
@@ -1,96 +1,96 @@
 angular.module('BaseClass', [])
-	.factory('BaseClass', function () {
+    .factory('BaseClass', function () {
 
-		'use strict';
+        'use strict';
 
-		var findObjectOfProperty = function (start, propName) {
-			while (true) {
-				if (!start) {
-					throw Error('Could not find property "' + propName + '"');
-				}
+        var findObjectOfProperty = function (start, propName) {
+            while (true) {
+                if (!start) {
+                    throw Error('Could not find property "' + propName + '"');
+                }
 
-				if (start.hasOwnProperty(propName)) {
-					return start;
-				}
+                if (start.hasOwnProperty(propName)) {
+                    return start;
+                }
 
-				start = Object.getPrototypeOf(start);
-			}
-		};
+                start = Object.getPrototypeOf(start);
+            }
+        };
 
-		// BaseClass constructor
-		var BaseClass = function (attributes) {
-			if (!angular.isObject(attributes)) {
-				throw Error('arguments provided to constructor must be a key/value pair object!');
-			}
+        // BaseClass constructor
+        var BaseClass = function (attributes) {
+            if (!angular.isObject(attributes)) {
+                throw Error('arguments provided to constructor must be a key/value pair object!');
+            }
 
-			angular.extend(this, attributes);
-		};
+            angular.extend(this, attributes);
+        };
 
-		BaseClass.extend = function (properties) {
+        BaseClass.extend = function (properties) {
 
-			var Class = this,
-				SubClass;
+            var Class = this,
+                SubClass;
 
-			if (angular.isObject(properties) && properties.hasOwnProperty('constructor')) {
-				SubClass = properties.constructor;
-			} else {
-				SubClass = function () {
-					return Class.apply(this, arguments);
-				}
-			}
+            if (angular.isObject(properties) && properties.hasOwnProperty('constructor')) {
+                SubClass = properties.constructor;
+            } else {
+                SubClass = function () {
+                    return Class.apply(this, arguments);
+                }
+            }
 
-			SubClass.extend = Class.extend;
+            SubClass.extend = Class.extend;
 
-			// Creates new Object based on Class prototype.
-			SubClass.prototype = Object.create(Class.prototype, {
-				constructor: { // Sets correct constructor on a SubClass prototype.
-					writable: true,
-					value: SubClass
-				},
-				_super: { // In case we need to access Class prototype from within a SubClass prototype.
-					value: function (method, args) {
-						var __super, previousSuper, start, superObject, returnValue;
+            // Creates new Object based on Class prototype.
+            SubClass.prototype = Object.create(Class.prototype, {
+                constructor: { // Sets correct constructor on a SubClass prototype.
+                    writable: true,
+                    value: SubClass
+                },
+                _super: { // In case we need to access Class prototype from within a SubClass prototype.
+                    value: function (method, args) {
+                        var __super, previousSuper, start, superObject, returnValue;
 
-						__super = '__super__' + method;
+                        __super = '__super__' + method;
 
-						previousSuper = this[__super];
+                        previousSuper = this[__super];
 
-						start = Object.getPrototypeOf(
-							previousSuper ? previousSuper : findObjectOfProperty(this, method) // where is the current method?
-						);
+                        start = Object.getPrototypeOf(
+                            previousSuper ? previousSuper : findObjectOfProperty(this, method) // where is the current method?
+                        );
 
-						superObject = findObjectOfProperty(start, method);
+                        superObject = findObjectOfProperty(start, method);
 
-						this[__super] = superObject;
+                        this[__super] = superObject;
 
-						returnValue = superObject[method].apply(this, args);
+                        returnValue = superObject[method].apply(this, args);
 
-						if (previousSuper) {
-							this[__super] = previousSuper;
-						} else {
-							delete this[__super];
-						}
+                        if (previousSuper) {
+                            this[__super] = previousSuper;
+                        } else {
+                            delete this[__super];
+                        }
 
-						return returnValue;
-					}
-				}
-			});
+                        return returnValue;
+                    }
+                }
+            });
 
-			angular.extend(SubClass.prototype, properties); // Extends SubClass prototype with supplied properties.
+            angular.extend(SubClass.prototype, properties); // Extends SubClass prototype with supplied properties.
 
-			if (Array.isArray(properties.mixins)) { // Add any mixin methods to SubClass prototype.
+            if (Array.isArray(properties.mixins)) { // Add any mixin methods to SubClass prototype.
 
-				properties.mixins.forEach(function (mixin) {
-					for (var prop in mixin) {
-						if (angular.isFunction(mixin[prop]) && angular.isUndefined(SubClass.prototype[prop])) {
-							SubClass.prototype[prop] = mixin[prop];
-						}
-					}
-				});
-			}
+                properties.mixins.forEach(function (mixin) {
+                    for (var prop in mixin) {
+                        if (angular.isFunction(mixin[prop]) && angular.isUndefined(SubClass.prototype[prop])) {
+                            SubClass.prototype[prop] = mixin[prop];
+                        }
+                    }
+                });
+            }
 
-			return SubClass;
-		};
+            return SubClass;
+        };
 
-		return BaseClass;
-	});
+        return BaseClass;
+    });
